466cf5df1b744357354424f0627782afe4da32bf
lopo <lopo@zju.edu.cn>
2022-04-29 17:30:51 +0800

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,37 @@
 <script lang="ts" setup>
+import { useAxiosStore } from "@/stores/axiosStore";
+import { setupConnect } from "@/scripts";
 
+const createRoomAndEnter = () => {
+    useAxiosStore()
+        .get('home', '/api/new-room')
+        .then(({ data }) => {
+            setupConnect(data.roomId)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
+
+const ping = () => {
+    useAxiosStore()
+        .get('home', '/ping')
+        .then(({ data }) => {
+            console.log(data)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
 </script>
 
 <template>
     <div class="home">
-        home page
+        <div class="new-room-btn" @click="createRoomAndEnter">
+            new room
+        </div>
+
+        <button @click="ping">ping</button>
     </div>
 </template>
 
@@ -13,5 +40,21 @@
     position: relative;
     width: 100%;
     height: 100%;
+    //display: flex;
+    //align-items: center;
+    //justify-content: center;
+
+    .new-room-btn {
+        position: relative;
+        width: fit-content;
+        height: 24px;
+        padding: 5px;
+        border: solid 1px #2da0ff;
+        border-radius: 5px;
+        background-color: white;
+        line-height: 24px;
+        user-select: none;
+        cursor: pointer;
+    }
 }
 </style>
