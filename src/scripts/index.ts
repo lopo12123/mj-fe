8645697b8002589ce5d036a0e636575fb90283e1
lopo <lopo@zju.edu.cn>
2022-04-29 17:30:44 +0800

    feat: 前端建立socket连接

---
+++ src/scripts/index.ts
@@ -0,0 +1,18 @@
+import { io } from "socket.io-client";
+import { useSocketStore } from "@/stores/socketStore";
+import { SocketType } from "@/types/io";
+
+const serverUrl = 'http://localhost:8899'
+
+/**
+ * @description 建立ws连接
+ */
+
+const setupConnect = (roomId: string) => {
+    const socket: SocketType = io(serverUrl, { query: { roomId }, withCredentials: true })
+    useSocketStore.updateSocket(socket)
+}
+
+export {
+    setupConnect
+}
