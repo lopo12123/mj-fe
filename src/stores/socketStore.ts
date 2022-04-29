69007b59090d769ebf899299c13f66e44ad960f5
lopo <lopo@zju.edu.cn>
2022-04-29 17:48:43 +0800

    docs: 添加注释

--- src/stores/socketStore.ts
+++ src/stores/socketStore.ts
@@ -4,6 +4,7 @@ class SocketStore {
     private socket: SocketType | null = null
 
     updateSocket(socket: SocketType | null) {
+        // 为了确保每次只存在一个socket连接, 则新建连接前断开已有连接(如果存在)
         this.socket?.disconnect()
         this.socket = socket
     }
